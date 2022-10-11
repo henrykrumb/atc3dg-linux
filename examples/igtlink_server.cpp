7b3f41d2ac49c81b34706c30d514e78df42197d1
Henry Krumb <mail@henrykrumb.de>
2022-10-11 10:08:15 +0200

    fix errors in igtlink server code

--- examples/igtlink_server.cpp
+++ examples/igtlink_server.cpp
@@ -11,13 +11,18 @@
 #include "igtlTrackingDataMessage.h"
 
 
-igtl::Socket::Pointer socket;
+igtl::Socket::Pointer client_socket;
 
 
-void signalHandler(int signum) {
-    if (socket.IsNotNull()) {
-        socket->CloseSocket();
+void cleanup() {
+    if (client_socket.IsNotNull()) {
+        client_socket->CloseSocket();
     }
+}
+
+
+void signal_handler(int signum) {
+    cleanup();
 }
 
 
@@ -25,9 +30,8 @@ int main(int argc, char* argv[]) {
     const int port = 18944;
     const int timeout = 1000;
 
-    igtl::ServerSocket::Pointer serverSocket;
-    serverSocket = igtl::ServerSocket::New();
-    int status = serverSocket->CreateServer(port);
+    auto server_socket = igtl::ServerSocket::New();
+    int status = server_socket->CreateServer(port);
 
     if (status < 0) {
         std::cerr << "Cannot create a server socket." << std::endl;
@@ -35,61 +39,67 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    auto tracker = ATC3DGTracker();
+    ATC3DGTracker tracker;
     const int num_sensors = tracker.get_number_sensors();
     const int interval = (int) (1000.0 / tracker.get_rate());
-    igtl::TrackingDataMessage::Pointer trackingMessage;
-    trackingMessage = igtl::TrackingDataMessage::New();
+    auto trackingMessage = igtl::TrackingDataMessage::New();
 
     for (int i = 0; i < num_sensors; i++) {
-        igtl::TrackingDataElement::Pointer trackingElement;
-        trackingElement = igtl::TrackingDataElement::New();
+        auto trackingElement = igtl::TrackingDataElement::New();
         std::string name = "Channel " + i;
         trackingElement->SetName(name.c_str());
         trackingElement->SetType(igtl::TrackingDataElement::TYPE_6D);
         trackingMessage->AddTrackingDataElement(trackingElement);
     }
 
-    double position[3];
-    double quaternion[4];
-    double atcmatrix[9];
-    double orientation[3];
+    double position[] = { 0.0, 0.0, 0.0 };
+    double quaternion[] = {0.0, 0.0, 0.0, 0.0};
+    double atcmatrix[] = {
+        0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0
+    };
+    double orientation[] = { 0.0, 0.0, 0.0 };
     double quality = 0;
     bool button = false;
+
+    float igtmatrix[4][4];
     igtl::TrackingDataElement::Pointer ptr;
 
-    signal(SIGINT, signalHandler);
+    bool running = true;
+    signal(SIGINT, signal_handler);
 
-    while (true) {
-        socket = serverSocket->WaitForConnection(timeout);
+    while (running)
+    {
+        client_socket = server_socket->WaitForConnection(timeout);
         
-        if (socket.IsNull()) {
+        if (client_socket.IsNull()) {
             continue;
         }
 
         for (int sensor = 0; sensor < num_sensors; sensor++) {
             tracker.update(sensor, position, orientation, atcmatrix, quaternion, &quality, &button);
             trackingMessage->GetTrackingDataElement(sensor, ptr);
-            float matrix[4][4];
+            
             for (int j = 0; j < 3; j++) {
                 for (int i = 0; i < 3; i++) {
-                    matrix[i][j] = static_cast<float>(atcmatrix[i + j * 3]);
+                    igtmatrix[i][j] = static_cast<float>(atcmatrix[i + j * 3]);
                 }
             }
-            matrix[3][0] = 0;
-            matrix[3][1] = 0;
-            matrix[3][2] = 0;
-            matrix[0][3] = position[0];
-            matrix[1][3] = position[1];
-            matrix[2][3] = position[2];
-            matrix[3][3] = 1.0;
-            ptr->SetMatrix(matrix);
+            igtmatrix[3][0] = 0.0f;
+            igtmatrix[3][1] = 0.0f;
+            igtmatrix[3][2] = 0.0f;
+            igtmatrix[0][3] = position[0];
+            igtmatrix[1][3] = position[1];
+            igtmatrix[2][3] = position[2];
+            igtmatrix[3][3] = 1.0f;
+            ptr->SetMatrix(igtmatrix);
         }
 
         trackingMessage->Pack();
-        socket->Send(trackingMessage->GetPackPointer(), trackingMessage->GetPackSize());
+        client_socket->Send(trackingMessage->GetPackPointer(), trackingMessage->GetPackSize());
         igtl::Sleep(interval);
     }
 
-    socket->CloseSocket();
+    cleanup();
 }
